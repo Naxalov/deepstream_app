f7e59a6496d9647b20c3dbf0f61b16c2b03fe037
Ubuntu <uzbzarif@gmail.com>
2025-02-07 16:41:30 +0500

    run custom image

--- deepstream_run.sh
+++ deepstream_run.sh
@@ -8,5 +8,5 @@ docker run --gpus all --detach --interactive --tty \
   --name deepstream_app \
   --volume "$(pwd)":/app \
   --workdir /app \
-  nvcr.io/nvidia/deepstream:7.1-gc-triton-devel \
+  deepstream_python_app:latest \
   bash
